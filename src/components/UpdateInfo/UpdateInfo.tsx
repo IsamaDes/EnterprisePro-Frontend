99d28266e72ab1dd451a1a5b094134e865ae630f
Desmond Isama <desmond.isama@decagon.dev>
2024-11-19 19:03:29 +0100

    update information

--- src/components/UpdateInfo/UpdateInfo.tsx
+++ src/components/UpdateInfo/UpdateInfo.tsx
@@ -3,19 +3,11 @@ import { Link } from 'react-router-dom';
 
 const UpdateInformation: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
         <h2 className="text-2xl font-bold mb-6">Update Information</h2>
         <div className="flex">
-          {/* Sidebar */}
-          <div className="w-1/4 pr-4">
-            <h3 className="text-lg font-bold mb-4">Account Settings</h3>
-            <ul>
-              <li><Link to="/kyc" className="btn btn-outline-secondary w-full mb-2">KYC</Link></li>
-              <li><Link to="/director-details" className="btn btn-outline-secondary w-full mb-2">Details of Director</Link></li>
-              <li><Link to="/admin-details" className="btn btn-outline-secondary w-full mb-2">Details of Admin</Link></li>
-            </ul>
-          </div>
+          
           {/* Main Content */}
           <div className="w-3/4">
             <h3 className="text-lg font-bold mb-4">What type of account do you want to create?</h3>
--- src/pages/UpdateInfoPage.tsx
+++ src/pages/UpdateInfoPage.tsx
@@ -1,8 +1,18 @@
 import UpdateInformation from '../components/UpdateInfo/UpdateInfo'
+import Sidebar from '../components/CompanyDashboard/Sidebar'
+import NavBar from '../components/CompanyDashboard/Navbar'
 
 function UpdateInfoPage() {
   return (
-    <UpdateInformation/>
+   
+    <div className="flex  ">
+      <Sidebar/>
+
+      <div className="flex flex-col w-full ">
+        <NavBar/>
+        <UpdateInformation/>
+        </div>
+    </div>
   )
 }
 
